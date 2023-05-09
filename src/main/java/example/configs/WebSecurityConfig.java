a8edc61f75d40805242375badd424ef7e30cee0f
DaJon Ewing <dajone37@gmail.com>
2023-05-08 20:05:46 -0700

    lab progress

---
+++ src/main/java/example/configs/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package example.configs;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    @Qualifier("userDetailsServiceImpl")
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/signup").permitAll()
+                        .requestMatchers("/css/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .and()
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsServiceImpl)
+                .passwordEncoder(passwordEncoder());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/example/repos/ApplicationUserRepository.java
@@ -0,0 +1,9 @@
+package example.repos;
+
+import example.models.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+     ApplicationUser findByUsername(String username);
+}
